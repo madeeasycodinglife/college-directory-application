f6e0319cfd070d85c6b5d1fbc15c7fff841ba36d
Pabitra Bera <madeeasycodinglife@gmail.com>
2024-09-27 17:03:49 +0530

    services set-up.

--- user-service/src/main/java/com/madeeasy/UserServiceApplication.java
+++ user-service/src/main/java/com/madeeasy/UserServiceApplication.java
@@ -4,10 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CollegeDirectoryApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(CollegeDirectoryApplication.class, args);
-	}
-
+public class UserServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UserServiceApplication.class, args);
+    }
 }
